b466f2e573f1166805cb9a6991cba24d2ece8c06
Lorenzo Ciani <lciani@pinkdonutstudio.com>
2025-11-12 13:08:51 +0100

    initial commit

---
+++ src/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify
+import datetime
+import socket
+
+app = Flask(__name__)
+
+@app.route('/api/v1/details')
+
+def details():
+    return jsonify({
+        'time': datetime.datetime.now().strftime("%d-%m-%Y"),   
+        'hostname': socket.gethostname()
+    }),200
+
+@app.route('/api/v1/healthz')
+
+def health():
+    return jsonify({
+        'status':'up'
+    }),200
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000)
